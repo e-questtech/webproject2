6e3e3dae80a4693de7d4e05740091a734a27e146
DanIzunna <105650690+DanIzunna@users.noreply.github.com>
2024-10-08 10:35:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -516,7 +516,7 @@ def sitemap():
 
 
 #Student Register (Prospective)
-@app.route('/register/', methods=['POST'])
+@app.route('/register/', methods=['POST', 'GET'])
 def student_register():
     full_name = request.form['fullName']
     email = request.form['email']
