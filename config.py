ce3e8d63cb2ebefa865976c635fc4c52ee85cd0d
DanIzunna <105650690+DanIzunna@users.noreply.github.com>
2024-10-09 10:18:02 +0100

    Update config.py added cloudinary

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 # config.py
 import os
 from dotenv import load_dotenv
+# Cloudinary Configuration
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 load_dotenv()
 class Config:
@@ -14,9 +18,19 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY','default_secret_key')
     
     UNSPLASH_ACCESS_KEY = os.getenv('UNSPLASH_ACCESS_KEY','9zXMTqIIAc2fz6iC4XZzsIcSI9TgSlzom7TzpKgvGJM')
-    
-    IMAGEKIT_PRIVATE_KEY=os.getenv('IMAGEKIT_PRIVATE_KEY'),
-    IMAGEKIT_PUBLIC_KEY=os.getenv('IMAGEKIT_PUBLIC_KEY'),
-    IMAGEKIT_URL_ENDPOINT=os.getenv('IMAGEKIT_URL_ENDPOINT')
+
+
+    CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
+
+    @staticmethod
+    def init_cloudinary():
+        cloudinary.config(
+            cloud_name=Config.CLOUDINARY_CLOUD_NAME,
+            api_key=Config.CLOUDINARY_API_KEY,
+            api_secret=Config.CLOUDINARY_API_SECRET
+        )
+
     
     
